52d683964e2d17646d6c26f13e9a9302de718cef
Julian Molineris <110922176+julianmolinerisit@users.noreply.github.com>
2023-10-26 22:29:43 -0300

    Update _app.js

--- mercadocarnes-frontend-main/src/pages/_app.js
+++ mercadocarnes-frontend-main/src/pages/_app.js
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import Layout from "../components/Layout";
-import "../../styles/globals.css";
 import store from "../../redux/store";
 import { Provider } from "react-redux";
+import "../../styles/globals.css"; // Asegúrate de mantener esta importación
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula un retraso para la pantalla de carga (por ejemplo, 5 segundos)
+    setTimeout(() => {
+      setIsLoading(false); // Cuando finaliza la carga, ocultamos la pantalla de carga
+    }, 5000); // Puedes ajustar el tiempo de carga aquí
+  }, []);
+
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {isLoading ? (
+        // Muestra la pantalla de carga aquí
+        <div className="loading-screen">
+          <img src="../../../img/preloader.jpg" alt="Cargando..." />
+        </div>
+      ) : (
+        // Renderiza el componente Layout y el contenido principal
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </Provider>
   );
 }
